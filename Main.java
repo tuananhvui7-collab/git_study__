bf35d5f8f455c211998a97fed7261a7496dbd2a6
tuananhvui7-collab <tuananh10042005@outlook.com>
2026-02-23 23:52:20 +0700

    commit version encapsulation + constructor

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
 public class Main {
     public static void main (String[] args){
-
     
-    Product prod1 = new Product();
-    Product prod2 = new Product();
+        Product prod1 = new Product("S1","sanpham1",50000.00,7);
+        Product prod2 = new Product("S2","sanpham2", 8000.00, 9);
 
-    prod1.id = "s1";
-    prod2.id = "s2";
-    // thiếu hàm main.
-        // làm sao để có thể gán cứng vào main.java?
-        // gán kiểu prod1.name kia gọi là hardcode, khi dữ liệu chưa dc đóng gói. 
-        // Đóng gói rồi chỉ dùng đc get set  va constructor.
+        // thiếu hàm main.
+            // làm sao để có thể gán cứng vào main.java?
+            // gán kiểu prod1.name kia gọi là hardcode, khi dữ liệu chưa dc đóng gói. 
+            // Đóng gói rồi chỉ dùng đc get set  va constructor.
 
-        System.out.println(prod1.id + " " + prod2.id);
+        System.out.println(prod1.getName() + " " + prod2.getName());
     }
 }
--- Product.java
+++ Product.java
@@ -1,9 +1,27 @@
 public class Product {
 
-    public String id;
-    public String name;
-    public Double price;
-    public int quantity;
+    private String id;
+    private String name;
+    private Double price;
+    private int quantity;
+
+    public Product(String id, String name, Double price, int quantity){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+
+    }
+
+    public void setName(String name){
+        if (name != null){
+            this.name = name;
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
 
 
 }
