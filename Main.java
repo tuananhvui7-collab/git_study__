34d3eba2351b5217e3b9fdbf23ff9d2f13abe86c
tuananhvui7-collab <tuananh10042005@outlook.com>
2026-02-26 09:13:59 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        Customer customer1 = new Customer(1000);
+        ArrayList<Smartphone> inventory = new ArrayList<>();
 
-        customer1.getBalance();
+        inventory.add(new Smartphone("ip", "ip1", 1002, 1));// add là add object chứ k phải là add String , phải tạo object mới. 
+        inventory.add(new Smartphone());
+        inventory.set(0, (new Smartphone("io" , "pps", 10132, 1))); //Hay bị indexOutOfBounce
+        inventory.size();
+        inventory.get(1);
+        inventory.remove(1); // 
+        inventory.remove("đt việt nam"); // hàm này trả false. vì k tìm thấy nen kệ?
+
+
+        for (Smartphone elements: inventory){
+            System.out.println(elements.getName()); // in luôn element thì lại ra địa chỉ??? Sửa thành getName xem sao
+            
+
+        }
+
+        }
         
-        customer1.topUp(500);
 
-        customer1.pay(20000);
 
     }
-}
