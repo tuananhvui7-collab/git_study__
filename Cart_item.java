8b4553b68e9acfc75975830372e90880637f2c87
tuananhvui7-collab <tuananh10042005@outlook.com>
2026-03-07 00:05:07 +0700

    commit đi

--- Cart_item.java
+++
@@ -1,8 +0,0 @@
-package git_study;
-public class Cart_item {
-    int quantity;
-
-    public void setQuantity(int quantity){ this.quantity = quantity;}
-    public int  getDescription(){ return quantity;}
-    
-}
